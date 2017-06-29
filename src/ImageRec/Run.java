b01e5eee381b3cce96f77367a5d6221858f4f597
Ryan Heaton <rheaton@10.144.5.44>
2017-06-29 09:43:43 -0400

    Made seeds

--- src/ImageRec/Run.java
+++ src/ImageRec/Run.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Run {
-
+	HashMap seedMap = new HashMap();
 	public static void main(String[] args) throws IOException {
 		BufferedImage img = ImageIO.read(new File("Resources/picsForHash/marble.jpg"));
 		System.out.println(new Color(img.getRGB(0, 0)));
@@ -17,8 +17,14 @@ public class Run {
 		System.out.println(img.getWidth()/((double) img.getHeight()));
 	}
 
-	public static void iteratePixels(BufferedImage image){
-		
+	public HashMap createSeeds(BufferedImage img){
+		for(int i = 0; i < img.getHeight() - 5; i++){
+			for(int n = 0; n < img.getWidth() - 5; n++){
+				//Seed seed = new Seed(i, n, i+5, n+5);
+				
+			}
+		}
+		return null;
 	}
 	
 	public static boolean compareSeeds(){
